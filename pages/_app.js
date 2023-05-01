a43e59646cb9c3364c8f79bc1809a77aa2556c74
vaz <vasanth93067@gmail.com>
2023-05-01 19:47:22 +0200

    Add Chakra UI

--- pages/_app.js
+++ pages/_app.js
@@ -1,18 +1,25 @@
-import "../styles/globals.css"
-import { Work_Sans } from "@next/font/google"
+import { Work_Sans } from "@next/font/google";
+import { ChakraProvider, extendTheme } from "@chakra-ui/react";
+import { withProse } from '@nikolovlazar/chakra-ui-prose';
 
 // importing the Work Sans font with
 // the Next.js 13 Font Optimization Feature
 const workSans = Work_Sans({
-  weight: ["400", "700"],
-  style: ["normal", "italic"],
-  subsets: ["latin"],
-})
+    weight: ["400", "700"],
+    style: ["normal", "italic"],
+    subsets: ["latin"],
+});
+
+
+const theme = extendTheme({}, withProse());
+
 
 function MyApp({ Component, pageProps }) {
-  return <main className={workSans.className}>
-    <Component {...pageProps} />
-  </main>
+    return (
+        <ChakraProvider theme={theme}>
+            <Component {...pageProps} />
+        </ChakraProvider>
+    );
 }
 
-export default MyApp
+export default MyApp;
