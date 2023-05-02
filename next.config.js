f77e7712d46a67d88813efa7b8df460d61ce1341
vaz <vasanth93067@gmail.com>
2023-05-02 03:14:18 +0200

    GH pages

--- next.config.js
+++ next.config.js
@@ -1,9 +1,19 @@
+const isProd = process.env.NODE_ENV === "production";
+
 module.exports = {
-  webpack: function(config) {
-    config.module.rules.push({
-      test: /\.md$/,
-      use: 'raw-loader',
-    })
-    return config
-  },
-}
+    assetPrefix: isProd ? "/kitchen-sink/" : "",
+    images: {
+        unoptimized: true,
+    },
+    output: 'export',
+    experimental: {
+      appDir: true,
+    },
+    webpack: function (config) {
+        config.module.rules.push({
+            test: /\.md$/,
+            use: "raw-loader",
+        });
+        return config;
+    },
+};
